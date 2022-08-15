c98e8b7cf264e680d161ea3977955741230b2815
한지선 <flqhs00@gmail.com>
2022-08-15 21:14:59 +0900

    feat: 유저에 따라 다른 알람 반환하도록 변경

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import { User } from '../domain/user';
-import { alarms } from './fakeData';
+import { returnAlarmsOfUser } from './fakeData';
 
 export function fakeApi<TResponse>(response: TResponse): Promise<TResponse> {
   return new Promise((res) => setTimeout(() => res(response), 450));
@@ -8,7 +8,7 @@ export function fakeApi<TResponse>(response: TResponse): Promise<TResponse> {
 export async function getAlarmsApi(user: User) {
   //fake call
   await sleep(1000);
-  return alarms;
+  return returnAlarmsOfUser(user);
 }
 
 export function sleep(timeout: number) {
--- src/services/fakeData.ts
+++ src/services/fakeData.ts
@@ -1,3 +1,19 @@
 import { Alarm } from '../domain/alarm';
+import { User } from '../domain/user';
 
-export const alarms: Alarm[] = new Array(10).fill(0).map((v, idx) => ({ id: idx, isOn: false }));
+const alarm1: Alarm[] = new Array(10).fill(0).map((v, idx) => ({ id: idx, isOn: false }));
+
+const alarm2: Alarm[] = new Array(10).fill(0).map((v, idx) => ({ id: idx + 100, isOn: false }));
+
+const alarm3: Alarm[] = new Array(10).fill(0).map((v, idx) => ({ id: idx + 200, isOn: false }));
+
+export const returnAlarmsOfUser = (user: User) => {
+  switch (true) {
+    case user.id.includes('1'):
+      return alarm1;
+    case user.id.includes('2'):
+      return alarm2;
+    default:
+      return alarm3;
+  }
+};
